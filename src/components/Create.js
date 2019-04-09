c9fbbe3a0d3dde4078cd1d639471e69d741db65a
Maria Micek <mmicek16@wp.pl>
2019-04-09 10:00:21 +0200

    Create component modified (version 2)

--- src/components/Create.js
+++ src/components/Create.js
@@ -2,12 +2,13 @@ import React from 'react'
 
 class Create extends React.Component {
     state = {
-        name: null,
-        age: null
+        name: '',
+        age: '',
+        color: ''
     }
 
-    handleChange = (event, fieldName) => {
-        this.setState({[fieldName]: event.target.value})
+    handleChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value })
     }
 
     render() {
@@ -16,15 +17,28 @@ class Create extends React.Component {
                 <div>
                     Name:
                 <input
+                        type={'text'}
+                        name={'name'}
                         value={this.state.name}
-                        onChange={event => this.handleChange(event, 'name')}
+                        onChange={this.handleChange}
                     />
                 </div>
                 <div>
                     Age:
                 <input
+                        type={'text'}
+                        name={'age'}
                         value={this.state.age}
-                        onChange={event => this.handleChange(event, 'name')}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div>
+                    Color:
+                <input
+                        type={'text'}
+                        name={'color'}
+                        value={this.state.color}
+                        onChange={this.handleChange}
                     />
                 </div>
                 <div>
